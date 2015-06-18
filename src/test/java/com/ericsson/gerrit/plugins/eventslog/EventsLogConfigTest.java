c65f3d198dea55050583040ba3eee641220c79c5
Janice Agustin <janice.agustin@ericsson.com>
2015-06-18 10:22:41 -0400

    Improve events-log config tests
    Change-Id: If340a388226b0d575e70c7ac959b58ad5d48e841

--- src/test/java/com/ericsson/gerrit/plugins/eventslog/EventsLogConfigTest.java
+++ src/test/java/com/ericsson/gerrit/plugins/eventslog/EventsLogConfigTest.java
@@ -14,22 +14,27 @@
 
 package com.ericsson.gerrit.plugins.eventslog;
 
-import static com.google.common.truth.Truth.assertThat;
 import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.CONFIG_CONN_TIME;
 import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.CONFIG_COPY_LOCAL;
 import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.CONFIG_DRIVER;
+import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.CONFIG_LOCAL_URL;
 import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.CONFIG_MAX_AGE;
+import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.CONFIG_MAX_TRIES;
 import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.CONFIG_PASSWORD;
 import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.CONFIG_RETURN_LIMIT;
 import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.CONFIG_URL;
 import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.CONFIG_URL_OPTIONS;
 import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.CONFIG_USERNAME;
+import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.CONFIG_WAIT_TIME;
 import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.DEFAULT_CONN_TIME;
 import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.DEFAULT_COPY_LOCAL;
 import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.DEFAULT_DRIVER;
 import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.DEFAULT_MAX_AGE;
+import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.DEFAULT_MAX_TRIES;
 import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.DEFAULT_RETURN_LIMIT;
 import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.DEFAULT_URL;
+import static com.ericsson.gerrit.plugins.eventslog.EventsLogConfig.DEFAULT_WAIT_TIME;
+import static com.google.common.truth.Truth.assertThat;
 import static org.easymock.EasyMock.expect;
 
 import com.google.gerrit.server.config.PluginConfig;
@@ -50,57 +55,74 @@ public class EventsLogConfigTest {
   private EventsLogConfig config;
   private PluginConfigFactory cfgFactoryMock;
   private PluginConfig configMock;
-  private PluginConfigFactory cfgFactoryMock2;
-  private PluginConfig configMock2;
+  private EasyMockSupport easyMock;
+  private String defaultStoreUrl;
+  private String localStoreUrl;
 
   @Rule
   public TemporaryFolder gerrit_site = new TemporaryFolder();
 
   @Before
   public void setUp() throws FileNotFoundException {
-    EasyMockSupport easyMock = new EasyMockSupport();
+    easyMock = new EasyMockSupport();
     site = new SitePaths(gerrit_site.getRoot());
     site.etc_dir.mkdirs();
-    configMock = easyMock.createNiceMock(PluginConfig.class);
+    defaultStoreUrl = "jdbc:h2:" + site.site_path.toString() + "/events-db/";
+    configMock = easyMock.createMock(PluginConfig.class);
+    cfgFactoryMock = easyMock.createMock(PluginConfigFactory.class);
+    expect(cfgFactoryMock.getFromGerritConfig(EasyMock.anyString(),
+        EasyMock.anyBoolean())).andStubReturn(configMock);
+  }
+
+  private void setUpDefaults() {
     expect(configMock.getBoolean(CONFIG_COPY_LOCAL, DEFAULT_COPY_LOCAL)).andReturn(DEFAULT_COPY_LOCAL);
     expect(configMock.getInt(CONFIG_MAX_AGE, DEFAULT_MAX_AGE)).andReturn(DEFAULT_MAX_AGE);
+    expect(configMock.getInt(CONFIG_MAX_TRIES, DEFAULT_MAX_TRIES)).andReturn(DEFAULT_MAX_TRIES);
     expect(configMock.getInt(CONFIG_RETURN_LIMIT, DEFAULT_RETURN_LIMIT)).andReturn(DEFAULT_RETURN_LIMIT);
     expect(configMock.getInt(CONFIG_CONN_TIME, DEFAULT_CONN_TIME)).andReturn(DEFAULT_CONN_TIME);
+    expect(configMock.getInt(CONFIG_WAIT_TIME, DEFAULT_WAIT_TIME)).andReturn(DEFAULT_WAIT_TIME);
     expect(configMock.getString(CONFIG_DRIVER, DEFAULT_DRIVER)).andReturn(DEFAULT_DRIVER);
     expect(configMock.getString(CONFIG_URL, DEFAULT_URL)).andReturn(DEFAULT_URL);
+    expect(configMock.getString(CONFIG_LOCAL_URL, defaultStoreUrl)).andReturn(defaultStoreUrl);
     expect(configMock.getString(CONFIG_URL_OPTIONS, "")).andReturn("");
     expect(configMock.getString(CONFIG_USERNAME)).andReturn(null);
     expect(configMock.getString(CONFIG_PASSWORD)).andReturn(null);
 
-    cfgFactoryMock = easyMock.createNiceMock(PluginConfigFactory.class);
-    expect(cfgFactoryMock.getFromGerritConfig(EasyMock.anyString(),
-        EasyMock.anyBoolean())).andStubReturn(configMock);
+    easyMock.replayAll();
+  }
 
-    configMock2 = easyMock.createNiceMock(PluginConfig.class);
-    expect(configMock2.getBoolean(CONFIG_COPY_LOCAL, DEFAULT_COPY_LOCAL)).andReturn(true);
-    expect(configMock2.getInt(CONFIG_MAX_AGE, DEFAULT_MAX_AGE)).andReturn(20);
-    expect(configMock2.getInt(CONFIG_RETURN_LIMIT, DEFAULT_RETURN_LIMIT)).andReturn(10000);
-    expect(configMock2.getInt(CONFIG_CONN_TIME, DEFAULT_CONN_TIME)).andReturn(5000);
-    expect(configMock2.getString(CONFIG_DRIVER, DEFAULT_DRIVER)).andReturn("org.h2.Driver2");
-    expect(configMock2.getString(CONFIG_URL, DEFAULT_URL)).andReturn("jdbc:h2:~/gerrit/db");
-    expect(configMock2.getString(CONFIG_URL_OPTIONS, "")).andReturn("DB_CLOSE_DELAY=10");
-    expect(configMock2.getString(CONFIG_USERNAME)).andReturn("testUsername");
-    expect(configMock2.getString(CONFIG_PASSWORD)).andReturn("testPassword");
-    cfgFactoryMock2 = easyMock.createNiceMock(PluginConfigFactory.class);
-    expect(cfgFactoryMock2.getFromGerritConfig(EasyMock.anyString(),
-        EasyMock.anyBoolean())).andStubReturn(configMock2);
+  private void setUpCustom() {
+    localStoreUrl = "jdbc:h2:~/gerrit/events-db/";
+    expect(configMock.getBoolean(CONFIG_COPY_LOCAL, DEFAULT_COPY_LOCAL)).andReturn(true);
+    expect(configMock.getInt(CONFIG_MAX_AGE, DEFAULT_MAX_AGE)).andReturn(20);
+    expect(configMock.getInt(CONFIG_MAX_TRIES, DEFAULT_MAX_TRIES)).andReturn(5);
+    expect(configMock.getInt(CONFIG_RETURN_LIMIT, DEFAULT_RETURN_LIMIT)).andReturn(10000);
+    expect(configMock.getInt(CONFIG_CONN_TIME, DEFAULT_CONN_TIME)).andReturn(5000);
+    expect(configMock.getInt(CONFIG_WAIT_TIME, DEFAULT_WAIT_TIME)).andReturn(5000);
+    expect(configMock.getString(CONFIG_DRIVER, DEFAULT_DRIVER)).andReturn("org.h2.Driver2");
+    expect(configMock.getString(CONFIG_URL, DEFAULT_URL)).andReturn("jdbc:h2:~/gerrit/db");
+    expect(configMock.getString(CONFIG_LOCAL_URL, defaultStoreUrl)).andReturn(localStoreUrl);
+    expect(configMock.getString(CONFIG_URL_OPTIONS, "")).andReturn("DB_CLOSE_DELAY=10");
+    expect(configMock.getString(CONFIG_USERNAME)).andReturn("testUsername");
+    expect(configMock.getString(CONFIG_PASSWORD)).andReturn("testPassword");
+
     easyMock.replayAll();
   }
 
   @Test
   public void shouldReturnDefaultsWhenMissingConfig() {
+    setUpDefaults();
     config = new EventsLogConfig(cfgFactoryMock, site, null);
     assertThat(config.getCopyLocal()).isFalse();
     assertThat(config.getMaxAge()).isEqualTo(30);
+    assertThat(config.getMaxTries()).isEqualTo(3);
     assertThat(config.getReturnLimit()).isEqualTo(5000);
     assertThat(config.getConnectTime()).isEqualTo(1000);
-    assertThat(config.getStoreDriver()).isEqualTo("org.h2.Driver");
-    assertThat(config.getStoreUrl()).isEqualTo("jdbc:h2:~/db/");
+    assertThat(config.getWaitTime()).isEqualTo(1000);
+    assertThat(config.getLocalStoreDriver()).isEqualTo(DEFAULT_DRIVER);
+    assertThat(config.getLocalStoreUrl()).isEqualTo(defaultStoreUrl);
+    assertThat(config.getStoreDriver()).isEqualTo(DEFAULT_DRIVER);
+    assertThat(config.getStoreUrl()).isEqualTo(DEFAULT_URL);
     assertThat(config.getUrlOptions()).isEmpty();
     assertThat(config.getStoreUsername()).isNull();
     assertThat(config.getStorePassword()).isNull();
@@ -108,11 +130,16 @@ public class EventsLogConfigTest {
 
   @Test
   public void shouldReturnConfigValues() {
-    config = new EventsLogConfig(cfgFactoryMock2, site, null);
+    setUpCustom();
+    config = new EventsLogConfig(cfgFactoryMock, site, null);
     assertThat(config.getCopyLocal()).isTrue();
     assertThat(config.getMaxAge()).isEqualTo(20);
+    assertThat(config.getMaxTries()).isEqualTo(5);
     assertThat(config.getReturnLimit()).isEqualTo(10000);
     assertThat(config.getConnectTime()).isEqualTo(5000);
+    assertThat(config.getWaitTime()).isEqualTo(5000);
+    assertThat(config.getLocalStoreDriver()).isEqualTo(DEFAULT_DRIVER);
+    assertThat(config.getLocalStoreUrl()).isEqualTo(localStoreUrl);
     assertThat(config.getStoreDriver()).isEqualTo("org.h2.Driver2");
     assertThat(config.getStoreUrl()).isEqualTo("jdbc:h2:~/gerrit/db");
     assertThat(config.getUrlOptions()).isEqualTo("DB_CLOSE_DELAY=10");
