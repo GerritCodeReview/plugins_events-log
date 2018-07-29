a19047c4b96d2c21ed39571934c7d19bbe83a79a
Hector Oswaldo Caballero <hector.caballero@ericsson.com>
2018-07-28 21:00:35 -0400

    Periodically remove older database entries
    So far, the removal of database entries older than the configured value
    of 'maxAge' was done synchronously at the plugin startup. This approach
    has two downsides: first, it can delay the plugin load and, second, if
    the plugin is not reloaded in a long time (which is desirable), the old
    entries accumulate on the database.
    
    Implement a periodic task that removes old entries from the database
    every day at 23 hours.
    
    Change-Id: I19e3ca9149e93fdb02f8630182b8bd85843611f0

--- src/main/java/com/ericsson/gerrit/plugins/eventslog/sql/EventsLogCleaner.java
+++ src/main/java/com/ericsson/gerrit/plugins/eventslog/sql/EventsLogCleaner.java
@@ -18,10 +18,18 @@ import com.ericsson.gerrit.plugins.eventslog.EventCleanerPool;
 import com.google.gerrit.extensions.events.ProjectDeletedListener;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 @Singleton
 public class EventsLogCleaner implements ProjectDeletedListener {
+  private static final int HOUR = 23;
+  private static final long INTERVAL = TimeUnit.DAYS.toSeconds(1);
+
   private final SQLClient eventsDb;
   private final SQLClient localEventsDb;
   private ScheduledThreadPoolExecutor pool;
@@ -44,5 +52,22 @@ public class EventsLogCleaner implements ProjectDeletedListener {
   public void removeProjectEventsAsync(String projectName) {
     pool.submit(() -> eventsDb.removeProjectEvents(projectName));
     pool.submit(() -> localEventsDb.removeProjectEvents(projectName));
+  }
+
+  public void scheduleCleaningWith(int maxAge) {
+    long initialDelay = getInitialDelay();
+    pool.scheduleAtFixedRate(
+        () -> eventsDb.removeOldEvents(maxAge), initialDelay, INTERVAL, TimeUnit.SECONDS);
+    pool.scheduleAtFixedRate(
+        () -> localEventsDb.removeOldEvents(maxAge), initialDelay, INTERVAL, TimeUnit.SECONDS);
+  }
+
+  private long getInitialDelay() {
+    ZonedDateTime now = ZonedDateTime.now(ZoneId.systemDefault());
+    ZonedDateTime next = now.withHour(HOUR).truncatedTo(ChronoUnit.HOURS);
+    if (now.isAfter(next)) {
+      next = next.plusDays(1);
+    }
+    return Duration.between(now, next).getSeconds();
   }
 }
